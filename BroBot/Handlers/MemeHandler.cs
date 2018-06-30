f1f2e59f06119ecd00a6fd3faf7dbf01d177ab9e
Matthew Joll <matthew@anacrusis.co.nz>
2018-07-01 00:00:16 +1200

    [master] Added meme support suing Bing search and cleaned imports in command classes.

---
+++ BroBot/Handlers/MemeHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace BroBot.Handlers
+{
+    class MemeHandler
+    {
+        private const string AccessKey = "access-code-here";
+        private const string UriBase = "https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=";
+        private const string SearchPrefix = "meme+";
+
+        public static string SearchMemeOnBing(string query)
+        {
+            var uriQuery = UriBase + SearchPrefix + Uri.EscapeDataString(query);
+
+            WebRequest req = HttpWebRequest.Create(uriQuery);
+            req.Headers["Ocp-Apim-Subscription-Key"] = AccessKey;
+
+            HttpWebResponse res = (HttpWebResponse)req.GetResponseAsync().Result;
+            string json = new StreamReader(res.GetResponseStream()).ReadToEnd();
+
+            JObject jsonObject = JObject.Parse(json);
+
+            return (string)jsonObject["value"][0]["contentUrl"];
+        }
+    }
+}
---
+++ BroBot/Modules/MemeCommands.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using BroBot.Handlers;
+using Discord;
+using Discord.Commands;
+
+namespace BroBot.Modules
+{
+    public class MemeCommands : ModuleBase<SocketCommandContext>
+    {
+        [Command("meme")]
+        public async Task GenerateMeme([Remainder]string message)
+        {
+            var result = MemeHandler.SearchMemeOnBing(message);
+
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Your meme, sir or ma'am.");
+            embed.WithColor(new Color(0, 255, 0));
+            embed.WithImageUrl(result);
+
+            await Context.Channel.SendMessageAsync("", false, embed);
+        }
+    }
+}
